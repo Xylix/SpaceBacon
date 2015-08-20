999115c78bb56c2d85c70cfe59e63cb98b6a36e2
LarePvl <laurivir@gmail.com>
2015-08-20 18:47:27 +0300

    Basic client stuff added.

---
+++ MainGame/client/screen.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#gamescreen").width = $(document).width();
+    $("#gamescreen").height = $(document).height();
+
+    $("#gamescreen").resize(function(){
+        $("#gamescreen").width = $(document).width();
+        $("#gamescreen").height = $(document).height();
+    });
+
+    Meteor.setTimeout(function() {
+        //MAIN function
+
+
+
+
+        
+    },500);
+
+})
