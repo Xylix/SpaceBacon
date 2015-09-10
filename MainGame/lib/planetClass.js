63a030c410ea5d85ee23245193a1d4267fff89b6
ehaihala <eero.haihala@paivola.fi>
2015-09-10 20:59:21 +0300

    working collision

--- MainGame/lib/planetClass.js
+++ MainGame/lib/planetClass.js
@@ -3,7 +3,7 @@ maps = new Meteor.Collection("maps")
 planet = function() {
     this.location = {x:Math.floor(Math.random()*4000)-2000,y:Math.floor(Math.random()*4000)-2000};
     this.radius = Math.ceil(250*(Math.random()+0.5));
-    this.mass = Math.ceil(this.radius/100);
+    this.mass = Math.ceil(this.radius*1000000);
     this.color = "rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")";
 }
 
--- MainGame/lib/playerClass.js
+++ MainGame/lib/playerClass.js
@@ -3,6 +3,7 @@ projectiles = new Meteor.Collection("projectiles");
 var maxSpeed = 10;
 
 player = function(name) {
+    this.moving = true;
     this.name = name;
     this.movedir = {x: 0, y: 0};
     this.location = {x: Math.floor(Math.random()*250), y: Math.floor(Math.random()*250)};
@@ -36,16 +37,29 @@ update = function(p){
         p.acceleration["x"] = p.movedir["x"]/Math.sqrt(2);
         p.acceleration["y"] = p.movedir["y"]/Math.sqrt(2);
     }
-
     if(p.speed["x"] + p.acceleration["x"] < maxSpeed){
         p.speed["x"] += p.acceleration["x"];
     }
     if(p.speed["y"] + p.acceleration["y"] < maxSpeed){
         p.speed["y"] += p.acceleration["y"];
     }
-
-    p.location["x"] += p.speed["x"];
-    p.location["y"] += p.speed["y"];
+    planets = getMap().planetList;
+    planets.forEach(function(name){
+        if (Math.sqrt(Math.pow(name.location.x - p.location.x, 2) + Math.pow(name.location.y - p.location.y, 2)) < 10000){
+            if (Math.sqrt(Math.pow(p.acceleration.x, 2) + Math.pow(p.acceleration.y, 2))<10){
+                p.acceleration.x += name.mass/Math.pow(name.location.x - p.location.x, 2);
+                p.acceleration.y += name.mass/Math.pow(name.location.y - p.location.y, 2);
+            }
+        }
+        if(Math.sqrt(Math.pow((p.location["x"] + p.speed["x"]) - name.location.x, 2) + Math.pow((p.location["y"] + p.speed["y"]) - name.location.y, 2)) < name.radius){
+            p.moving = false;
+        }
+    });
+    if (p.moving){
+        p.location["x"] += p.speed["x"];
+        p.location["y"] += p.speed["y"];
+    }
+    p.moving = true;
 
     p.speed["x"] /= 1.1;
     if (Math.abs(p.speed["x"]) < 0.1){
@@ -55,16 +69,13 @@ update = function(p){
     if (Math.abs(p.speed["y"]) < 0.1){
         p.speed["y"] = 0;
     }
-
-    p.location["x"] += p.speed["x"];
-    p.location["y"] += p.speed["y"];
-
     plrs.update({_id: p._id}, {$set: {acceleration: p.acceleration, speed: p.speed, location: p.location}});
 }
 
 shoot = function(player, point){
-    console.log("angle is: " + String(Math.atan2(point.x, point.y)))
+    console.log("angle is: " + String(Math.atan2((w/2)-point.x, (h/2)-point.y)))
     // projectiles.insert({
     //     location: player.location,
+    //     angle: Math.atan2((w/2)-point.x, (h/2)-point.y)
     // })
 }
